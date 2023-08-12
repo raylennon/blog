a80ec476b5c8c50be9b69710b6e392af6d2d245c
Ray Lennon <raylennon27@gmail.com>
2023-08-12 17:55:05 -0400

    cv update

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.get('/community', (req, res) => {
 });
 
 const pdfRoutes = {
-  CV: 'RayLennon_CV_07-23-2023.pdf',
+  CV: 'RayLennon_CV_081123.pdf',
   PhototherapyPoster: 'PhototherapyPoster.pdf',
   CDCPaper: 'From Hospitals to Hurricanes - How APL Is Using Computational Fluid Dynamics to Inform the Future of Public Health and Safety.pdf'
 };
