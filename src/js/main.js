ed0109ca8eab2a4ff950432961e002ee0d0b7c0a
Ray Lennon <raylennon27@gmail.com>
2024-08-22 11:48:41 -0400

    misc updates

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,21 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import 'bootstrap/js/dist/modal';
+
+import {  Toast } from 'bootstrap';
+
 // import 'bootstrap/js/dist/navbar';
+
+const toastTriggers = document.querySelectorAll('[id^="toast"]');
+const toastLiveExample = document.getElementById('ToastNotif');
+
+if (toastTriggers.length > 0) {
+//   console.log("Toast triggered...");
+  const toastBootstrap = Toast.getOrCreateInstance(toastLiveExample);
+//   navigator.clipboard.writeText(window.location.href);
+  toastTriggers.forEach(toastTrigger => {
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show();
+    });
+  });
+}
